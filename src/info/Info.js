5e7a6ea3533b75bcfe2d80cb6cfa9a8d4fa772f5
catastrxphic <mcopo001@fiu.edu>
2023-08-10 19:43:40 -0500

    installed and ran npm

--- src/info/Info.js
+++ src/info/Info.js
@@ -1,31 +1,27 @@
 import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import mock1 from "../img/mock1.webp"
+import mock2 from "../img/mock2.webp"
+import mock3 from "../img/mock3.webp"
 
-
-// export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-export let colors = ["rgb(229, 143, 178)", "rgb(107, 46, 142)"];
+export let colors = ["rgb(214, 158, 181)", "rgb(155, 146, 221)"];
 
 
 export const info = {
     firstName: "Cami",
     lastName: "Copo Amador",
-    initials: "CCA", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Computer Science and Asian Studies Studend",
-    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    initials: "CCA", 
+    position: "a Computer Science and Asian Studies Student",
+    selfPortrait: self, 
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, 
     baseColor: colors[0],
-    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+    miniBio: [ 
         {
             emoji: '☕',
             text: 'fueled by coffee'
         },
         {
-            emoji: '🌎',
-            text: 'based in the US'
+            emoji: '🏫',
+            text: 'Florida International University'
         },
         {
             emoji: "💼",
@@ -33,15 +29,10 @@ export const info = {
         },
         {
             emoji: "📧",
-            text: "mcamilacoa@gamil.com"
+            text: "mcamilacoa@gmail.com"
         }
     ],
     socials: [
-        {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
         {
             link: "https://www.instagram.com/_catastrophic._/",
             icon: 'fa fa-instagram',
@@ -56,17 +47,10 @@ export const info = {
             link: "https://www.linkedin.com/in/camicopoa/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
-// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-// Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hey There! I love technology, especially coding and ML/AI :) /br Curious, open-minded, and a fast learner. I want to find as many opportunities as possible to learn about new things. Fun fact? I also love reading and learning languages.",
+    bio: "Hey There! I love technology, especially coding and ML/AI : Curious, open-minded, and a fast learner. I want to find as many opportunities as possible to learn about new things. Fun fact? I also love reading and learning languages.",
     skills:
         {
             proficientWith: ['python', 'git', 'github', 'html5', 'css3', 'figma', 'java', 'dash'],
@@ -115,18 +99,6 @@ export const info = {
             source: "https://devpost.com/software/litterdetect",
             image: mock3
         }
-        // },
-        // {
-        //     title: "Project 4",
-        //     live: "https://paytonpierce.dev",
-        //     source: "https://github.com/paytonjewell",
-        //     image: mock4
-        // },
-        // {
-        //     title: "Project 5",
-        //     live: "https://paytonpierce.dev",
-        //     source: "https://github.com/paytonjewell",
-        //     image: mock5
-        // }
+    
     ]
 }
