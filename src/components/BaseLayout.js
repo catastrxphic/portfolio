f33208a50ccb131076f9a7605a318860934782a4
catastrxphic <mcopo001@fiu.edu>
2023-08-12 19:55:27 -0400

    working on imports for particles, added and commented firebase needed code

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -7,38 +7,52 @@ import Portfolio from "./portfolio/Portfolio";
 import {Route, Routes} from "react-router-dom";
 import {Box, Grid} from "@mui/material";
 import ParticlesBg from './Assets/ParticlesBG';
+// import { logGa } from '../log/log';
+
 
 export default function BaseLayout() {
    let [darkMode, setDarkMode] = useState(false);
 
-   function handleToggleDarkMode() {
-      let oppositeOfCurrentDarkMode = !darkMode
-      console.log(oppositeOfCurrentDarkMode)
-      localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`)
-      setDarkMode(oppositeOfCurrentDarkMode)
-   }
+  function handleToggleDarkMode() {
+    let oppositeOfCurrentDarkMode = !darkMode;
+    localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`);
+    setDarkMode(oppositeOfCurrentDarkMode);
+   //  logGa('dark_mode_toggle', oppositeOfCurrentDarkMode ? 'dark' : 'light');
+  }
 
-   useEffect(() => {
-      let detectedDarkMode = eval(localStorage.getItem('darkMode'));
+  useEffect(() => {
+    let detectedDarkMode = JSON.parse(localStorage.getItem('darkMode'));
+    const osDarkModeQuery = window.matchMedia
+      ? window.matchMedia('(prefers-color-scheme: dark)')
+      : null;
 
-      if (detectedDarkMode) {
-         setDarkMode(detectedDarkMode)
-      } else {
-         localStorage.setItem('darkMode', 'false')
-      }
-   }, [])
+    if (detectedDarkMode) {
+      setDarkMode(detectedDarkMode);
+    } else {
+      localStorage.setItem('darkMode', `${!!osDarkModeQuery?.matches}`);
+    }
+
+    const updateDarkMode = (e) => {
+      setDarkMode(e.matches);
+    };
+
+    if (osDarkModeQuery) {
+      osDarkModeQuery.addEventListener('change', updateDarkMode);
+    }
+  }, []);
 
    return (
       <Box className={darkMode ? Style.dark : Style.light}>
          <ParticlesBg darkMode={darkMode.valueOf()} />
          <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
                justifyContent={'space-between'}>
+               
             <Grid item>
                <Navbar darkMode={darkMode} handleClick={handleToggleDarkMode}/>
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
-                  <Route exact path={'/'} element={<Home/>}/>
+                  <Route exact path={'/'} element={<Home darkMode={darkMode}/>} />
                   <Route exact path={'/about'} element={<About/>}/>
                   <Route exact path={'/portfolio'} element={<Portfolio/>}/>
                </Routes>
