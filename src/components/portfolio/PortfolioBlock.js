0581d4b1871817a7a314e7f759cab0f5a44342e9
catastrxphic <mcopo001@fiu.edu>
2023-08-13 00:37:23 -0400

    deleted unused live variable

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -3,7 +3,7 @@ import IconLink from "./IconLink";
 import {Box} from "@mui/material";
 
 function PortfolioBlock(props) {
-   const {image, live, source, title} = props;
+   const {image, source, title} = props;
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
         <Box
@@ -22,11 +22,6 @@ function PortfolioBlock(props) {
          />
          <h1 style={{ fontSize: '2rem' }}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'} alignItems={'center'} fontSize={'1.5rem'} py={'2rem'} >
-            {/* {live && (
-               <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
-                  <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'} />
-               </Box>
-            )} */}
 
          {source && (
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
